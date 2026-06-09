7fef14f7800c981329dc5bbf1a44168bdef3c09b
RandomBrushes <brushes@mailfence.com>
2026-06-09 18:42:11 +0200

    Minor formatting fix

--- im_mip.cc
+++ im_mip.cc
@@ -403,7 +403,8 @@ bool MIP_ProcessImage(const char *filename)
   COL_SetFullBright(fullbright);
 
   // Quake1: If the texture names starts with "{", then enable transparency
-  if(game_type == GAME_Quake1 && lump_name.rfind("{", 0) == 0) {
+  if(game_type == GAME_Quake1 && lump_name.rfind("{", 0) == 0)
+  {
     COL_SetTransparent(255);
   }
 
